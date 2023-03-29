cfc018b5d3cb92efe9ae57dd02dbe31eec6aa745
Nobab <nobabsarkar2016@gmail.com>
2023-03-29 22:56:28 +0600

    router setup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./Components/Navbar/Navbar";
-import Footer from "./Components/Footer/Footer";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router/Router";
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Navbar></Navbar>
-      <Footer></Footer>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
