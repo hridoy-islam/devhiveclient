bcd4398eaad7baa50c073f5da91886eb5e7193f2
PRONOY <pronoybanik82@gmail.com>
2023-03-30 19:53:41 +0600

    slider complite

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import CarouselSlider from '../Components/CarouselSlider/CarouselSlider';
 import Services from '../Components/Services/Services';
 
 const Home = () => {
     return (
         <div>
+            <CarouselSlider></CarouselSlider>
             <Services></Services>
         </div>
     );
