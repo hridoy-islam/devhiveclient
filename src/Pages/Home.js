f1265fc7c3fa7decc3c5237c71e5648b1f4352b8
mushfik16381 <ahmed.mushfik16381@gmail.com>
2023-03-30 16:31:18 +0600

    info & marketplace section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Info from '../Components/Info/Info';
+import MarketPlace from '../Components/MarketPlace/MarketPlace';
 
 const Home = () => {
     return (
         <div>
-            Home
+            
         </div>
     );
 };
