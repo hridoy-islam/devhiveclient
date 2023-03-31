2a3bf4698f567154983266a505ab7f2459caa52e
Mr. Hasan <hasanshahriar32@gmail.com>
2023-03-31 12:49:36 +0600

    login page finalized

--- src/Components/LoginProviders/LoginProviders.js
+++ src/Components/LoginProviders/LoginProviders.js
@@ -2,13 +2,25 @@ import React from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const LoginProviders = () => {
+  const handleGoogleLogin = () => {
+    console.log("google login");
+  };
+  const handleGithubLogin = () => {
+    console.log("github login");
+  };
   return (
     <div className="flex flex-col gap-2">
-      <button className="btn flex gap-5 w-full btn-outline btn-error">
+      <button
+        className="btn flex gap-5 w-full btn-outline btn-error"
+        onClick={handleGoogleLogin}
+      >
         <FaGoogle></FaGoogle>
         <span>Continue with Google</span>
       </button>
-      <button className="btn w-full flex gap-5 btn-outline ">
+      <button
+        className="btn w-full flex gap-5 btn-outline "
+        onClick={handleGithubLogin}
+      >
         <FaGithub></FaGithub>
         <span>Continue with GitHub</span>
       </button>
