0957876dccb549adcbefd108db62c729eac0cded
Hridoy Islam <hridoy4t@gmail.com>
2023-04-03 11:21:05 +0600

    latest

--- src/features/Store.js
+++ src/features/Store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import apiSlice from './api/apislice';
+import apiSlice from './api/apiSlice';
 import loginReducer from './api/loginSlice'
 export default configureStore({
   reducer: {
