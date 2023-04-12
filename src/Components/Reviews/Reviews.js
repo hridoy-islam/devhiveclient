19ae8a9a8af61b6e5c53585a8593eb2548d3a1be
Mr. Hasan <hasanshahriar32@gmail.com>
2023-04-13 00:30:38 +0600

    Reviews component added to take user review

---
+++ src/Components/Reviews/Reviews.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+const Reviews = () => {
+  return (
+    <div>
+      <div
+        data-tf-popover="PTSqlNUt"
+        data-tf-custom-icon="https://images.typeform.com/images/LcKqMJzA3c26"
+        data-tf-opacity="100"
+        data-tf-iframe-props="title=DevHive Project Ratings"
+        data-tf-transitive-search-params
+        data-tf-button-color="transparent"
+        data-tf-medium="snippet"
+      ></div>
+      <Helmet>
+        <script src="//embed.typeform.com/next/embed.js"></script>
+      </Helmet>
+    </div>
+  );
+};
+
+export default Reviews;
