f61fe5cae7c7aadb85e20cbf51c9abcc13a87574
Mr. Hasan <hasanshahriar32@gmail.com>
2023-04-18 14:28:51 +0600

    about page updated

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -4,6 +4,13 @@ import Reviews from "../../Components/Reviews/Reviews";
 const About = () => {
   return (
     <div>
+      <div className="flex items-center justify-center h-60">
+        <a target="_blank" href="https://hasanshahriar32.github.io/devhive">
+          <button className="btn btn-xl btn-secondary btn-outline">
+            Visit The Documentation
+          </button>
+        </a>
+      </div>
       <Reviews></Reviews>
     </div>
   );
