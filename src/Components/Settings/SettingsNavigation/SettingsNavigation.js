c039148f210c7d284cb0c3bad1dfa9a8ee4d633c
Mr. Hasan <hasanshahriar32@gmail.com>
2023-04-19 12:10:08 +0600

    updated settings

--- src/Components/Settings/SettingsNavigation/SettingsNavigation.js
+++ src/Components/Settings/SettingsNavigation/SettingsNavigation.js
@@ -9,8 +9,8 @@ const SettingsNavigation = () => {
           to="/settings/account"
           className={({ isActive }) =>
             isActive
-              ? "flex items-center gap-2 border-l-[3px] border-accent-content bg-primary px-4 py-3 text-accent-content"
-              : "flex items-center gap-2 border-l-[3px] border-transparent px-4 py-3 text-gray-500 hover:border-gray-100 hover:bg-gray-50 hover:text-gray-700"
+              ? "flex items-center gap-2 border-l-[3px] border-secondary text-secondary bg-primary  px-4 py-3 "
+              : "flex items-center gap-2 border-l-[3px] border-transparent px-4 py-3 text-secondary hover:border-gray-100 hover:bg-gray-50 hover:text-secondary"
           }
         >
           <svg
@@ -34,8 +34,8 @@ const SettingsNavigation = () => {
           to="/settings/security"
           className={({ isActive }) =>
             isActive
-              ? "flex items-center gap-2 border-l-[3px] border-accent-content bg-primary px-4 py-3 text-accent-content"
-              : "flex items-center gap-2 border-l-[3px] border-transparent px-4 py-3 text-gray-500 hover:border-gray-100 hover:bg-gray-50 hover:text-gray-700"
+              ? "flex items-center gap-2 border-l-[3px] border-secondary text-secondary bg-primary  px-4 py-3 "
+              : "flex items-center gap-2 border-l-[3px] border-transparent px-4 py-3 text-secondary hover:border-gray-100 hover:bg-gray-50 hover:text-secondary"
           }
         >
           <svg
@@ -64,8 +64,8 @@ const SettingsNavigation = () => {
           to="/settings/notification"
           className={({ isActive }) =>
             isActive
-              ? "flex items-center gap-2 border-l-[3px] border-accent-content bg-primary px-4 py-3 text-accent-content"
-              : "flex items-center gap-2 border-l-[3px] border-transparent px-4 py-3 text-gray-500 hover:border-gray-100 hover:bg-gray-50 hover:text-gray-700"
+              ? "flex items-center gap-2 border-l-[3px] border-secondary text-secondary bg-primary  px-4 py-3 "
+              : "flex items-center gap-2 border-l-[3px] border-transparent px-4 py-3 text-secondary hover:border-gray-100 hover:bg-gray-50 hover:text-secondary"
           }
         >
           <svg
@@ -89,8 +89,8 @@ const SettingsNavigation = () => {
           to="/settings/business_info"
           className={({ isActive }) =>
             isActive
-              ? "flex items-center gap-2 border-l-[3px] border-accent-content bg-primary px-4 py-3 text-accent-content"
-              : "flex items-center gap-2 border-l-[3px] border-transparent px-4 py-3 text-gray-500 hover:border-gray-100 hover:bg-gray-50 hover:text-gray-700"
+              ? "flex items-center gap-2 border-l-[3px] border-secondary text-secondary bg-primary  px-4 py-3 "
+              : "flex items-center gap-2 border-l-[3px] border-transparent px-4 py-3 text-secondary hover:border-gray-100 hover:bg-gray-50 hover:text-secondary"
           }
         >
           <svg
@@ -115,8 +115,8 @@ const SettingsNavigation = () => {
           to="/settings/payment"
           className={({ isActive }) =>
             isActive
-              ? "flex items-center gap-2 border-l-[3px] border-accent-content bg-primary px-4 py-3 text-accent-content"
-              : "flex items-center gap-2 border-l-[3px] border-transparent px-4 py-3 text-gray-500 hover:border-gray-100 hover:bg-gray-50 hover:text-gray-700"
+              ? "flex items-center gap-2 border-l-[3px] border-secondary text-secondary bg-primary  px-4 py-3 "
+              : "flex items-center gap-2 border-l-[3px] border-transparent px-4 py-3 text-secondary hover:border-gray-100 hover:bg-gray-50 hover:text-secondary"
           }
         >
           <svg
@@ -136,29 +136,29 @@ const SettingsNavigation = () => {
 
           <span className="text-sm font-medium"> Payment Methods</span>
         </NavLink>
-        <div className="divider"></div> 
+        <div className="divider"></div>
         <NavLink
           to="/admin"
           className={({ isActive }) =>
             isActive
-              ? "flex items-center gap-2 border-l-[3px] border-accent-content bg-primary px-4 py-3 text-accent-content"
-              : "flex items-center gap-2 border-l-[3px] border-transparent px-4 py-3 text-gray-500 hover:border-gray-100 hover:bg-gray-50 hover:text-gray-700"
+              ? "flex items-center gap-2 border-l-[3px] border-secondary text-secondary bg-primary  px-4 py-3 "
+              : "flex items-center gap-2 border-l-[3px] border-transparent px-4 py-3 text-secondary hover:border-gray-100 hover:bg-gray-50 hover:text-secondary"
           }
         >
-           <svg
-                      xmlns="http://www.w3.org/2000/svg"
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5 opacity-75"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
             strokeWidth="2"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
 
           <span className="text-sm font-medium">Administrator</span>
         </NavLink>
