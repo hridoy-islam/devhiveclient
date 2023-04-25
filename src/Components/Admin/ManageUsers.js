ade3c0c0e1fd5c7926052ebf53c3dbe6d0b3802b
Mr. Hasan <hasanshahriar32@gmail.com>
2023-04-25 22:32:41 +0600

    loading animation added to admin routes

--- src/Components/Admin/ManageUsers.js
+++ src/Components/Admin/ManageUsers.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import LoadingAnimation from "../LoadingAnimation";
 
 const ManageUsers = () => {
   const [userData, setUserData] = useState();
@@ -53,83 +54,87 @@ const ManageUsers = () => {
 
   return (
     <div className="w-full lg:w-[750px]">
-      <div className="overflow-x-auto w-full">
-        <table className="table w-full">
-          {/* head */}
-          <thead>
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <th>Name</th>
-              <th>Credential</th>
-              <th>About</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            {userData?.map((userData) => (
-              <tr
-                data-aos-duration="3000"
-                data-aos="fade-left"
-                key={userData._id}
-              >
+      {!userData ? (
+        <LoadingAnimation></LoadingAnimation>
+      ) : (
+        <div className="overflow-x-auto w-full">
+          <table className="table w-full">
+            {/* head */}
+            <thead>
+              <tr>
                 <th>
                   <label>
-                    <input type="checkbox" className="checkbox " />
+                    <input type="checkbox" className="checkbox" />
                   </label>
                 </th>
-                <td>
-                  <div className="flex items-center space-x-3">
-                    <div className="avatar">
-                      <div className="mask mask-squircle w-12 h-12">
-                        <img src={userData?.pic} />
+                <th>Name</th>
+                <th>Credential</th>
+                <th>About</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {userData?.map((userData) => (
+                <tr
+                  data-aos-duration="3000"
+                  data-aos="fade-left"
+                  key={userData._id}
+                >
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox " />
+                    </label>
+                  </th>
+                  <td>
+                    <div className="flex items-center space-x-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                          <img src={userData?.pic} />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{userData?.name}</div>
+                        <div className="text-sm opacity-50">United States</div>
                       </div>
                     </div>
-                    <div>
-                      <div className="font-bold">{userData?.name}</div>
-                      <div className="text-sm opacity-50">United States</div>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  {userData?.email}
-                  <br />
-                  <span className="badge badge-ghost badge-sm">
-                    {userData?.uid}
-                  </span>
-                </td>
-                <td>Buyer</td>
-                <th>
-                  <button className="btn btn-ghost btn-xs">
-                    Make Verified
-                  </button>
-                  <button
-                    onClick={() => handleDelete(userData?._id)}
-                    className="btn btn-danger btn-xs"
-                  >
-                    Delete
-                  </button>
-                </th>
+                  </td>
+                  <td>
+                    {userData?.email}
+                    <br />
+                    <span className="badge badge-ghost badge-sm">
+                      {userData?.uid}
+                    </span>
+                  </td>
+                  <td>Buyer</td>
+                  <th>
+                    <button className="btn btn-ghost btn-xs">
+                      Make Verified
+                    </button>
+                    <button
+                      onClick={() => handleDelete(userData?._id)}
+                      className="btn btn-danger btn-xs"
+                    >
+                      Delete
+                    </button>
+                  </th>
+                </tr>
+              ))}
+              {/* row 2 */}
+            </tbody>
+            {/* foot */}
+            <tfoot>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Credential</th>
+                <th>About</th>
+                <th></th>
               </tr>
-            ))}
-            {/* row 2 */}
-          </tbody>
-          {/* foot */}
-          <tfoot>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Credential</th>
-              <th>About</th>
-              <th></th>
-            </tr>
-          </tfoot>
-        </table>
-      </div>
+            </tfoot>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
