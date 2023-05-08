0cb0c6fd02ad123c12b3bafdf5c8c953ec5e4a5f
Mr. Hasan <hasanshahriar32@gmail.com>
2023-05-08 12:09:21 +0600

    dev register update

--- src/Pages/DeveloperProfile/DeveloperProfile.js
+++ src/Pages/DeveloperProfile/DeveloperProfile.js
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from "react";
 import DeveloperIntro from "../../Components/DeveloperProfile.js/DeveloperIntro";
 import axios from "axios";
-import { useLoaderData } from "react-router-dom";
-
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
 
 const DeveloperProfile = () => {
   const profile = useLoaderData([]);
-  
-  
+  console.log(profile);
+  let location = useLocation();
+  const from = location.state?.from?.pathname || "/developer-register";
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (profile.length == 0) {
+      navigate(from, { replace: true });
+    }
+  }, [profile]);
+
   return (
     <div>
       <DeveloperIntro profile={profile}></DeveloperIntro>
