24a5b35759aa6fc4ecddfd60cbd1129167e6c4fc
Mr. Hasan <hasanshahriar32@gmail.com>
2023-05-09 18:34:04 +0600

    updated color theme

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -15,7 +15,7 @@ const Profile = () => {
   const userData = useSelector((state) => state.login.userData);
   return (
     <div>
-      <NavThemeToggle></NavThemeToggle>
+      {/* <NavThemeToggle></NavThemeToggle> */}
       <h1 className="text-3xl text-center font-bold m-5">Profile</h1>
       <div className="grid grid-cols-12 lg:m-6 m-auto px-auto ">
         <div className="md:col-span-5 col-span-11 flex flex-col items-center justify-center gap-10 ">
